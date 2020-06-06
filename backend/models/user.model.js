0b308e8b88f71dd67f4dde5b8ff9097ffd925bee
Cryosith <caseyvandyke@ymail.com>
2020-06-06 11:23:34 -0600

    added register and login validation

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,18 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minLength: 3
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-}, {
-    timestamps: true 
-});
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -6,11 +7,17 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+//Bodyparser middleware parses to json
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+app.use(bodyParser.json)
 app.use(morgan('dev'));
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;  // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.use(cors());
-app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
